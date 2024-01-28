bbb502684c8fa1d3645263fec221c186ff2ea399
Daniil Klyutko <klyutko.d@gmail.com>
2024-01-28 23:43:29 +0600

    edit color of spinner

--- components/UI/Spinner.vue
+++ components/UI/Spinner.vue
@@ -2,7 +2,7 @@
   <div role="status" class="flex items-center justify-center h-screen w-full">
     <svg
       aria-hidden="true"
-      class="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-yellow-400"
+      class="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-yellow-500"
       viewBox="0 0 100 101"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
